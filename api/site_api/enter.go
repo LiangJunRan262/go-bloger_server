fc6a44bdbeb089a1b6fefe28425331148d54e3d5
LiangJunRan262 <18835130915@163.com>
2025-05-07 23:37:09 +0800

    feat:登录日志设置

--- api/site_api/enter.go
+++ api/site_api/enter.go
@@ -1,6 +1,9 @@
 package site_api
 
 import (
+	"bloger_server/models/enum"
+	log_service "bloger_server/service/log_servive"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,6 +11,8 @@ type SiteApi struct {
 }
 
 func (SiteApi) SiteInfoView(c *gin.Context) {
+	log_service.NewLoginSuccess(c, enum.UserPwdLoginType)
+	log_service.NewLoginFail(c, enum.UserPwdLoginType, "登录失败", "admin", "123456")
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
